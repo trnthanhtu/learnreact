cfced6945192211cc5cc1b30df2505b24de0042e
at-tutt <tranthanhtu10t1bkdn@gmail.com>
2017-04-04 19:28:22 +0700

    Props and event

--- public/xuly.js
+++ public/xuly.js
@@ -1,9 +1,17 @@
 var KhoaPham = React.createClass({
+  laythongtin: function(){
+      alert(this.props.children);
+  },
+
   render: function(){
     return(
       <div>
         <h1 className="mauvang"> {this.props.ten} - {this.props.giangvien} </h1>
         <KhoaHoc />
+        <div>
+          {this.props.children}
+        </div>
+        <button onClick={this.laythongtin}>Click me!</button>
       </div>
     );
   }
@@ -19,7 +27,7 @@ var KhoaHoc = React.createClass({
 
 ReactDOM.render(
   <div>
-    <KhoaPham ten="Reactjs" giangvien="Mr. A"/>
-    <KhoaPham ten="Nodejs" giangvien="Mr. B"/>
+    <KhoaPham ten="Reactjs" giangvien="Mr. A">Hi</KhoaPham>
+    {/*<KhoaPham ten="Nodejs" giangvien="Mr. B"/>*/}
   </div>
   , document.getElementById("root"));
